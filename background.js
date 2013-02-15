1944297f51af8fcbe8c61fc19517c0057a88c27b
Petter Hareim <phareim@gmail.com>
2013-02-15 09:02:00 +0100

    init versjon 9

---
+++ background.js
@@ -0,0 +1,103 @@
+/**
+ *
+ * check out the reference for the google-feed-library:
+ * http://code.google.com/apis/ajaxfeeds/documentation/reference.html#JSON
+ *
+ **/
+
+google.load("feeds", "1");
+
+var RN = {}; 
+RN.interval = 10000;
+RN.unread = 0;
+var telegraph_feed;
+
+function telegraph_feed(){
+	return telegraph_feed;
+}
+
+function test(){
+	pop(telegraph_feed.entries[0]);
+}
+
+function pop(entry){
+	var notification = webkitNotifications.createNotification( 
+		'http://forum.spilltelegrafen.no/themes/Spilltelegrafen/design/images/authors/'+entry.author+'.png', 
+		entry.title, 
+		unescape(entry.contentSnippet) );
+		notification.addEventListener('click', function() {
+	    notification.cancel();
+	    window.open(entry.link);
+		});
+	notification.show();	
+	setTimeout(function(){
+  	notification.cancel();
+	}, 5000);
+
+
+}
+
+//Load the list of posts we've seen  
+RN.seen = localStorage["seen"];
+if(!RN.seen){
+    RN.seen = [];
+}
+else{
+    RN.seen = JSON.parse(RN.seen);
+}
+
+function setUnread(num){
+	RN.unread = num;
+	chrome.browserAction.setBadgeText({"text":""+RN.unread});
+}
+
+//Add a post to the list of posts we've seen
+function addSeen(id){
+    RN.seen.push(id);
+    localStorage["seen"] = JSON.stringify(RN.seen);
+}
+
+//List of feeds
+
+//Initialize the list of feeds
+function initializeFeeds(){
+	RN.feed = (new google.feeds.Feed("http://forum.spilltelegrafen.no/discussions/comments/all/feed.rss"));
+}
+
+function onFeedLoad(result){
+		telegraph_feed = result.feed;
+    console.log("Checking feed...");
+    if(!result.error){
+        var container = document.getElementById("feed");
+        for (var i = 0; i < result.feed.entries.length; i++) {
+            var entry = result.feed.entries[i];
+            //If we have not seen this post
+            if(RN.seen.indexOf(entry.link) < 0){
+            		setUnread(RN.unread+1);
+                pop(entry);
+                addSeen(entry.link);
+            }
+        }
+    }
+    else{
+        console.error("There was an error loading the feed.")
+    }
+}
+
+//Load a feed and check it for new posts
+function loadFeed(){
+		RN.feed.setNumEntries(RN.unread > 5? RN.unread: 5);
+    RN.feed.load(onFeedLoad);
+    //Run again next interval
+    setTimeout(loadFeed, RN.interval);
+}
+
+//Initialize the feeds and start the regular monitoring
+function start(){
+    initializeFeeds();
+    loadFeed();
+    console.log("Started checking feeds...");
+}
+
+//Once the Google Feeds API starts check the feeds
+google.setOnLoadCallback(start);
---
+++ popup.js
@@ -0,0 +1,57 @@
+var bg = chrome.extension.getBackgroundPage();
+
+function OnLoad() {
+  presentFeed(chrome.extension.getBackgroundPage().telegraph_feed); 
+  bg.setUnread(0);
+}
+
+//setInterval(presentFeed(chrome.extension.getBackgroundPage().telegraph_feed), 10000);
+
+google.setOnLoadCallback(OnLoad);
+
+function presentFeed(result) {
+  if (!result.error) {
+    var container = document.getElementById("content");
+  	container.innerHTML = '';
+    for (var i = result.entries.length - 1; i >= 0; i--) {
+    	container.appendChild(createPopUpEntry( result.entries[i]) );
+    };
+
+    var clear = document.createElement('div');
+    clear.setAttribute('class','clear');
+    container.appendChild(clear);
+  }
+}
+
+function createPopUpEntry(entry){
+	var ent = document.createElement("div");
+	ent.setAttribute('class','popup-entry rounded');
+	var a = document.createElement('a');
+	a.title = entry.title;
+	a.setAttribute('class','popup-title');
+	a.innerHTML = entry.author +": "+entry.title;
+	a.href = entry.link;
+	a.setAttribute('target','_blank');
+	ent.appendChild(a);
+	ent.appendChild(document.createElement('br'));
+	/*var createddate = document.createElement('p');
+	createddate.innerHTML = entry.pubDate;
+	ent.appendChild(document.createElement('br'));*/
+	var image = document.createElement("img");
+	image.setAttribute('class','popup-avatar');
+	image.setAttribute('src','images/'+entry.author+'.png');
+	ent.appendChild(image);
+	var content = document.createElement("div");
+	content.setAttribute('class','popup-content');
+	//var snippet = document.createElement('p');
+	//snippet.setAttribute('class','snippet');
+	//snippet.innerHTML = unescape(entry.contentSnippet);
+	content.innerHTML = unescape(entry.contentSnippet);
+	//var mouseover = document.createElement('div');
+	//mouseover.setAttribute('class','mouseover');
+	//mouseover.innerHTML = entry.content;
+	//content.appendChild(snippet);
+	//content.appendChild(mouseover);
+	ent.appendChild(content);
+	return ent;
+}
